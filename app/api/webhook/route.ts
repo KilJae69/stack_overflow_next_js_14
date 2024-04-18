0bfd2493f60ddd13234e4733a095318fee865df5
KilJae69 <adi.toromanovic@outlook.com>
2024-04-18 20:26:31 +0200

    ...

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -5,6 +5,7 @@ import { WebhookEvent } from "@clerk/nextjs/server";
 import { createUser, deleteUser, updateUser } from "@/lib/actions/user.action";
 import { NextResponse } from "next/server";
 
+console.log("ff")
 export async function POST(req: Request) {
   // You can find this in the Clerk Dashboard -> Webhooks -> choose the webhook
   const WEBHOOK_SECRET = process.env.NEXT_CLERK_WEBHOOK_SECRET;
