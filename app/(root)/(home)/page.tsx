ea2125c3ef5de769be0a405c6518448ec17d42eb
KilJae69 <adi.toromanovic@outlook.com>
2024-06-21 10:34:45 +0200

    pagination

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -2,6 +2,7 @@ import QuestionCard from "@/components/cards/QuestionCard";
 import HomeFilters from "@/components/home/HomeFilters";
 import Filter from "@/components/shared/Filter";
 import NoResult from "@/components/shared/NoResult";
+import Pagination from "@/components/shared/Pagination";
 import LocalSearchbar from "@/components/shared/search/LocalSearchbar";
 import { Button } from "@/components/ui/button";
 import { HomePageFilters } from "@/constants/filters";
@@ -10,14 +11,13 @@ import { SearchParamsProps } from "@/types";
 import Link from "next/link";
 import React from "react";
 
-
-const HomePage = async ({searchParams}:SearchParamsProps) => {
-
+const HomePage = async ({ searchParams }: SearchParamsProps) => {
   const result = await getQuestions({
     searchQuery: searchParams.q,
-  })
+    filter: searchParams.filter,
+  });
+  // Fetch recommended questions
 
- 
   return (
     <>
       <div className="flex w-full flex-col-reverse justify-between gap-4 sm:flex-row sm:items-center">
@@ -49,26 +49,32 @@ const HomePage = async ({searchParams}:SearchParamsProps) => {
       <div className="mt-10 flex w-full flex-col gap-6">
         {result.questions.length > 0 ? (
           result.questions.map((question) => (
-          <QuestionCard 
-          key={question._id}
-          _id={question._id}
-          title={question.title}
-          tags={question.tags}
-          author={question.author}
-          upvotes={question.upvotes}
-          views={question.views}
-          answers={question.answers}
-          createdAt={question.createdAt}
-          />
-        ))
+            <QuestionCard
+              key={question._id}
+              _id={question._id}
+              title={question.title}
+              tags={question.tags}
+              author={question.author}
+              upvotes={question.upvotes}
+              views={question.views}
+              answers={question.answers}
+              createdAt={question.createdAt}
+            />
+          ))
         ) : (
           <NoResult
             title="There's no questions to show"
             description="Be the first to break the silence! 🚀 Ask a Question and kickstart the discussion. our query could be the next big thing others learn from. Get involved! 💡"
             link="/ask-question"
-            linkTitle = "Ask a Question"
+            linkTitle="Ask a Question"
           />
         )}
+      </div>
+      <div className="mt-10">
+        <Pagination 
+        pageNumber = {searchParams?.page ? +searchParams.page : 1}
+        isNext = {result.isNext}
+        />
       </div>
     </>
   );
